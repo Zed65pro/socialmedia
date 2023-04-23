8c1cca52913190f52d04cb02770d21a3b1772de8
zed65pro <zed65pro@hotmail.com>
2023-04-23 17:33:45 +0400

    k

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Signin from "./components/templates/Signin/Signin";
 import Signup from "./components/templates/Signup/Signup";
 import Home from "./components/pages/Home";
+import { constants } from "./constants";
 import "./app.css";
 
 const App = () => {
   return (
     <Routes>
-      <Route exact path="/signup" element={<Home />} />
-      <Route exact path="/signin" element={<Signin />} />
-      <Route path="/" element={<Navigate to="/signin" />} />
+      <Route exact path={`${constants.BASE_URL}/signup`} element={<Home />} />
+      <Route exact path={`${constants.BASE_URL}/signin`} element={<Signin />} />
+      <Route
+        path={`${constants.BASE_URL}`}
+        element={<Navigate to={`${constants.BASE_URL}/signin`} />}
+      />
     </Routes>
   );
 };
